c4cac751ae499b876eb80cb267a346e81eb4a622
abhijitak11 <42020956+abhijitak11@users.noreply.github.com>
2020-03-18 20:14:00 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,30 @@
+import json
+import requests
+#sort the list of tuples in descending order
+def Sort_Tuple(repo_fork):   
+    repo_fork.sort(key = lambda x: x[1],reverse=True)  
+    return repo_fork 
+#inputs
+orgname = input("Enter the Organisation name:")
+number_of_repo = int(input("Enter number of popular repositories:"))
+number_of_committees = int(input("Enter number of popular committees:"))
+
+r = requests.get('https://api.github.com/orgs/'+orgname+'/repos')
+todos = json.loads(r.text)
+repo_fork = [(sub['full_name'],sub['forks_count']) for sub in todos] 
+Sort_Tuple(repo_fork)
+k=1
+for i in repo_fork[0:number_of_repo]:
+    print("Repository number:",k,i[0]," ","Fork counts:",i[1])
+    k+=1
+    l = requests.get('https://api.github.com/repos/'+i[0]+'/contributors')
+    temp = json.loads(l.text)
+    contributors_commits = [(sub['login'],sub['contributions']) for sub in temp]
+    Sort_Tuple(contributors_commits)
+    m=1
+    for j in contributors_commits[0:number_of_committees]:
+        print("Github id:",m,j[0]," ","Commit counts:",j[1])
+        m+=1
+    print()
+    print()
+
